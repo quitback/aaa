a5ac933b9d27e2b3d0d270e1eae4f70f1b3d1477
quitback <naspalow@mail.ru>
2025-07-16 01:12:28 +0700

    added some basic word

---
+++ main.py
@@ -0,0 +1,69 @@
+
+import pyautogui 
+import os
+import cv2
+import base64
+from datetime import timedelta,timezone,datetime
+import time
+from time import sleep
+from tqdm import tqdm
+import time
+import keyboard
+import subprocess
+import os
+
+
+# file = "key_press.txt"
+
+# def click_enbt(event):
+#     with open(file,"a",encoding="utf-8") as f:
+#         f.write(f"{event.name}\n")
+    
+# def start():
+#     keyboard.on_press(click_enbt)
+#     keyboard.wait("esc")
+    
+# if __name__ == "__main__":
+#     start()
+
+
+
+
+
+
+
+
+# def zxc (dirs = ".",text = "zcx",expanders=(".txt",)):
+#     for i in os.listdir(dirs):
+#         if i.endswith(expanders):
+#             filepath = os.path.join(dirs,i)
+#             with open(filepath,"w",encoding="utf-8") as f:
+#                 f.write(text)
+                
+# zxc(expanders=(".txt",".py",".cpp"))
+
+# cap = cv2.VideoCapture(0)
+
+# while True:
+#     ret, img = cap.read()
+#     cv2.imshow("camera",img)
+
+
+# pyautogui.FAILSAFE = True
+
+
+
+# path = os.getcwd()
+# print(path)
+    
+
+# print(os.environ["APPDATA"])
+
+
+
+# while True:
+#     sleep(4)
+#     pyautogui.scroll(-500)
+#     sleep(15)
+#     pyautogui.scroll(-500)
+#     continue
